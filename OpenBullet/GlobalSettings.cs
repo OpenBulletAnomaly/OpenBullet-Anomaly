8331a528cf165a7ca4871e9db4de65129fc7b565
OpenBulletAnomaly <110566590+OpenBulletAnomaly@users.noreply.github.com>
2022-08-03 18:29:49 -0500

    Add files via upload

---
+++ OpenBullet/GlobalSettings.cs
@@ -0,0 +1,13 @@
+﻿using RuriLib.Interfaces;
+using RuriLib.Models;
+using RuriLib.ViewModels;
+
+namespace OpenBullet
+{
+    public class GlobalSettings : ISettings
+    {
+        public EnvironmentSettings Environment { get; set; }
+        public RLSettingsViewModel RLSettings { get; set; }
+        public ProxyManagerSettings ProxyManagerSettings { get; set; }
+    }
+}
---
+++ OpenBullet/MainDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenBullet
+{
+    /// <summary>
+    /// Logica di interazione per MainDialog.xaml
+    /// </summary>
+    public partial class MainDialog : Window
+    {
+        public MainDialog(Page content, string title)
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Content = content;
+            Title = title;
+        }
+    }
+}
